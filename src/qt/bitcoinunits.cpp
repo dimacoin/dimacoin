d69b60988bbb789fc0a51da6dbfd1f882eda64f5
dimacoin <dima@unic.ac.cy>
2014-01-31 18:01:51 +0200

    changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case BTC: return QString("DimaCoin");
+    case mBTC: return QString("Milli-DimaCoin (1 / 1,000)");
+    case uBTC: return QString("Micro-DimaCoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
